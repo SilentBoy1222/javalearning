d03bfd9df984035e904f14ff61f9e177618ec88d
SilentBoy1222 <1981223386@qq.com>
2025-11-06 20:34:00 +0800

    a change

---
+++ src/practise/InputTest.java
@@ -0,0 +1,15 @@
+package practise;
+import java.util.*;
+public class InputTest 
+{
+	public static void main(String[]args)
+	{
+		Scanner in=new Scanner(System.in);
+		System.out.print("what is your name?");
+		String name=in.nextLine();
+		System.out.print("how old are you?");
+		int age =in.nextInt();
+		System.out.print("hello,"+name+".Next year,you will be"+(age+1));
+	}
+
+}
